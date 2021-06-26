507ff4153e5542f317f825ba37e247de224c084b
chuefengv <feng.vang98@gmail.com>
2021-06-26 00:11:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Axios from 'axios';
+import {useEffect} from 'react';
 function App() {
+  const API_KEY = '1364ff82ee89c8513773028b0e6a5191'
+
+  useEffect(()=>{
+    Axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=${API_KEY}`)    
+    .then(res =>{
+        console.log(res.data.list[0]); //every 8 indexes is a new day
+    })
+    .catch(err =>{
+        console.log(err.message);
+    });
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
