9958e7fa3804fd5a53487b1c2134d530ffcc04b1
chuefengv <feng.vang98@gmail.com>
2021-06-28 00:27:54 -0400

    moved axios to seperate function file

--- src/components/OpeningPage/OpeningPage.jsx
+++ src/components/OpeningPage/OpeningPage.jsx
@@ -1,24 +1,11 @@
 import React, {useState, useEffect}  from 'react'
-import Axios from 'axios';
+import searchWeather from '../searchWeather.js'
+import Axios from 'axios'
 import './OpeningPage.css'
 
 export default function OpeningPage({setFirstSearch, weather}) {
     const [zipcode, setZipcode] = useState(0);
-    const API_KEY = '1364ff82ee89c8513773028b0e6a5191'
-
-    
-    function searchWeather(){
-
-        Axios.get(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode}&appid=${API_KEY}`)    
-        .then(res =>{
-            setFirstSearch(true);
-            weather.setCurrWeather(res.data.city.name);
-            console.log(weather.currWeather); //every 8 indexes is a new day
-        })
-        .catch(err =>{
-            console.log(err.message);
-        });        
-    }
+    const [wrongInput, setWrongInput] = useState(false);
     
     function handleZipChange(e){
         e.preventDefault();
@@ -29,12 +16,13 @@ export default function OpeningPage({setFirstSearch, weather}) {
         <div className='opening-wrapper'>
             <div className='opening-searchbox'>
 
+                {wrongInput && <div>Not a Valid Zipcode</div>}
                 <div className='opening-search-city'>
-                    Enter Zip Code
+                    <p>Enter Zip Code</p>
                 </div>
                 <input type='text' placeholder='90210' onChange={(e)=>handleZipChange(e)} maxLength="5"/>
 
-                <button onClick={()=>searchWeather()}>Search</button>
+                <button onClick={()=>searchWeather(zipcode, weather, setFirstSearch, setWrongInput)}>Search</button>
             </div>
         </div>
     )
